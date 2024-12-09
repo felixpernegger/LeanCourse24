32f8b3491d2cf19421e9561f42d3be0de3b5ac9e
Felix Pernegger <s59fpern@uni-bonn.de>
2024-12-09 23:11:23 +0100

    t

--- LeanCourse/Project/Circumcircle.lean
+++ LeanCourse/Project/Circumcircle.lean
@@ -73,9 +73,68 @@ lemma pmidpoint_same_abs (a b : Point): point_abs a (pmidpoint a b) = point_abs 
 
 /-The midpoint is the only point with the last two properties:-/
 
-theorem pmidpoint_unique {a b p : Point}(h : colinear a b p)(hp : point_abs a p = point_abs p b): p = pmidpoint a b := by{
-  by_cases ab: a=b
+theorem pmidpoint_unique {a b p : Point}(ab : a ≠ b)(h : colinear a b p)(hp : point_abs a p = point_abs p b): p = pmidpoint a b := by{
+  have : in_between a b p ∨ in_between b p a ∨ in_between p a b := by{exact colinear_imp_in_between2 a b p h}
+  obtain h0|h0|h0 := this
+  swap
+  unfold in_between at *
+  rw[hp] at h0
+  rw[point_abs_symm p b] at h0
+  simp at h0
+  contrapose ab
+  simp at *
+  symm
+  exact abs_zero_imp_same b a h0
 
+  swap
+  unfold in_between at *
+  rw[point_abs_symm p a] at h0
+  rw[hp] at h0
+  rw[point_abs_symm p b] at h0
+  simp at h0
+  contrapose ab
+  simp at *
+  symm
+  exact abs_zero_imp_same b a h0
+
+  unfold in_between at h0
+  have ap: point_abs a p = 1/2 * point_abs a b := by{
+    rw[← hp] at h0
+    linarith
+  }
+  have col2: colinear a p (pmidpoint a b) := by{
+    have col1: colinear a b (pmidpoint a b) := by{exact pmidpoint_colinear a b}
+    have ba: b ≠ a := by{exact id (Ne.symm ab)}
+    apply colinear_perm12 at col1
+    apply colinear_perm12 at h
+    exact colinear_trans b a p (pmidpoint a b) h col1 ba
+  }
+  have : in_between a p (pmidpoint a b) ∨ in_between p (pmidpoint a b) a ∨ in_between (pmidpoint a b) a p := by{exact colinear_imp_in_between2 a p (pmidpoint a b) col2}
+  obtain p0|p0|p0 := this
+  unfold in_between at p0
+  rw[ap, pmidpoint_abs_left] at p0
+  simp at p0
+  symm
+  exact abs_zero_imp_same (pmidpoint a b) p p0
+
+  unfold in_between at p0
+  rw[point_abs_symm p a, ap, pmidpoint_abs_left] at p0
+  apply colinear_perm23 at h
+  apply (colinear_alt2 a p b).1 at h
+  simp [*] at *
+  obtain ⟨t,ht⟩ := h
+
+
+  sorry
+
+
+
+
+  unfold in_between at p0
+  rw[point_abs_symm p a, ap, point_abs_symm (pmidpoint a b) a, pmidpoint_abs_left] at p0
+  simp at p0
+  symm
+  exact abs_zero_imp_same (pmidpoint a b) p p0
 }
 
 def perp_bisector{a b : Point}(ab : a ≠ b) : Line :=
@@ -147,7 +206,7 @@ theorem perp_bisector_def (a b p : Point)(ab : a ≠ b): (point_abs p a = point_
     simp at this
     assumption
   }
-  apply pmidpoint_unique this
+  apply pmidpoint_unique ab this
   rw[point_abs_symm]
   assumption
 
