8a4627b45dd8c23e889d4ef77ee521defad06f51
Felix Pernegger <s59fpern@uni-bonn.de>
2024-12-11 23:07:48 +0100

    ooh

--- LeanCourse/Project/Tangents.lean
+++ LeanCourse/Project/Tangents.lean
@@ -39,7 +39,18 @@ theorem tangent_point_unique{L : Line}{C : CCircle}(p : Point)(h : Tangent L C)(
 
 /-therefore the distance on the tangent point is the radius of the circle to the center:-/
 
-lemma point_abs_tangent_point{L : Line}{C : CCircle}(h : Tangent L C) : 0=0 := by{
+lemma point_abs_tangent_point{L : Line}{C : CCircle}(h : Tangent L C) : point_abs (Center C) (Tangent_point h) = Radius C := by{
+  refine point_abs_point_lies_on_circle (Tangent_point h) ?h
+  exact tangent_point_on_circle h
+}
+
+/-Also note:-/
+lemma tangent_point_foot{L : Line}{C : CCircle}(h : Tangent L C) : Tangent_point h = foot (Center C) L := by{
+  symm
+  apply tangent_point_unique
+  constructor
+  · exact foot_on_line L (Center C)
+  refine point_on_circle_simp ?hp.right.h
   sorry
 }
 
@@ -50,5 +61,10 @@ A line is tangent to a circle, iff the foot of the center of the Circle lies on 
 theorem line_tangent_iff(L : Line)(C : CCircle): Tangent L C ↔ Lies_on_circle (foot (Center C) L) C := by{
   constructor
   intro h
-  have
+  refine point_on_circle_simp ?mp.h
+  rw[← tangent_point_foot h]
+  exact point_abs_tangent_point h
+
+  intro h
+  sorry
 }
