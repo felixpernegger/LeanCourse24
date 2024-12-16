1ab7e515d8772a21f67ab44ca18e9e83c57b80dd
Felix Pernegger <s59fpern@uni-bonn.de>
2024-12-16 02:04:34 +0100

    t

--- LeanCourse/Project/Angles.lean
+++ LeanCourse/Project/Angles.lean
@@ -1,4 +1,4 @@
-import LeanCourse.Project.Circumcircle
+import LeanCourse.Project.Powpoint
 import Mathlib
 
 open Function Set Classical
@@ -9,3 +9,13 @@ noncomputable section
 --Angles
 
 #check Complex.arg
+
+/-We now FINALLY deifne angles (I pretty much did everything you can reasonably do without angles lol)-/
+
+/-We will use angles only as directed angles, furthermore, for the time being,
+we only define angles between three points. Definining angles between lines is a bit tricky.
+
+We will also make two versions of angles, seen as followed:
+-/
+#check Angle
+def Angle
